c88d003c20ba51fd8fed81d11bfedeaafa8fdf88
Sanchir01 <emgushovs@mail.ru>
2024-10-26 18:26:22 +0300

    fix path in useRedux hook

--- src/shared/hooks/useRedux.ts
+++ src/shared/hooks/useRedux.ts
@@ -3,7 +3,7 @@ import {
 	useDispatch,
 	useSelector
 } from 'react-redux'
-import type { AppDispatch, RootState } from '~/app/index.ts'
+import type { AppDispatch, RootState } from '~/app/store'
 
 export const useTypedSelector: TypedUseSelectorHook<RootState> = useSelector
 export const useAppDispatch = () => useDispatch<AppDispatch>()
